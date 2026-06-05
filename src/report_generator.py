8e1ec7508e711e3aa9f6d2987f135c509797e370
Misbaulhuda <misbahuda912@gmail.com>
2026-06-05 13:07:35 +0530

    Create report_generator.py

---
+++ src/report_generator.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+    generate_student_report(
+        "S001",
+        78.5,
+        ["Algebra", "Probability"]
+    )
+
+    generate_student_report(
+        "S002",
+        65.0,
+        ["Geometry", "Trigonometry"]
+    )
+
+    print("Reports generated successfully!")
